88ea46f42c3dbcb44ea4394e52523610f80cccfa
Samuel Butta <samuel@butta.cz>
2017-11-18 22:14:04 +0100

    template for group of todos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,42 @@
                             Wellcome user
                         @endif
                     </div>
+
                 </div>
+
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">List name</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                       aria-describedby="emailHelp">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleTextarea">Description</label>
+                                <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-lg">Save</button>
+                        </form>
+                    </div>
+                </div>
+
+                <hr>
+
+
+                <div class="row list-group center-block text-center">
+
+                    <div class="list-group-item">
+                        <div class="card-block">
+                            <h3 class="card-title">Special title treatment</h3>
+                            <p class="card-text">With </p>
+                            <a href="#" class="btn btn-success">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
